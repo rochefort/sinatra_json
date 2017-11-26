c5930853234d19fa38701b3dad6b28cd51232a00
rochefort <terasawan@gmail.com>
2017-11-27 06:56:06 +0900

    Add cors example

--- lib/app.rb
+++ lib/app.rb
@@ -10,5 +10,6 @@ get "/json/*" do |json_file|
   json_path = File.join("public", json_file)
   pass unless File.exist?(json_path)
   # do something
+  # response["Access-Control-Allow-Origin"] = "*"
   json JSON.parse(open(json_path).read)
 end
